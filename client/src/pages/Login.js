aef3dd5d68ae9da85dfed1fb8195e5e55dfc09dc
Monica Gerard <mlgvla@gmail.com>
2022-08-27 22:08:50 -0400

    stub Home and Login pages and components

---
+++ client/src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import LoginForm from '../components/LoginForm'
+import SignUpForm from '../components/SignUpForm'
+
+function Login() {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
