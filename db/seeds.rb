d765cbc2584568fb24c20ff0321a8d40e3061c0f
Monica Gerard <mlgvla@gmail.com>
2022-08-28 00:59:07 -0400

    add users to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+u1 = User.create(username: "monica", email: "monica@me.com", password: "123")
+u2 = User.create(username: "tom", email: "tom@me.com", password: "456")
+u3 = User.create(username: "nate", email: "nate@me.com", password: "789")
+u4 = User.create(username: "seyr", email: "seyr@me.com", password: "061")
+u5 = User.create(username: "amanda", email: "amanda@me.com", password: "987")
