95b8ba6370215b3df58195f6bf55e5052a1ef1c6
Monica Gerard <mlgvla@gmail.com>
2022-09-03 19:08:19 -0400

    add rest-client

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
 import { BrowserRouter } from "react-router-dom"
+import { ChakraProvider } from "@chakra-ui/react"
 import reportWebVitals from "./reportWebVitals"
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <ChakraProvider>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </ChakraProvider>
 )
 
 // If you want to start measuring performance in your app, pass a function
